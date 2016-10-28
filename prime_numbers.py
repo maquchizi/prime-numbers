df794d58c3d4327cc6dff520dc7133dbe697ce44
Mark Ng'ang'a <maqnganga@gmail.com>
2016-10-28 16:39:45 +0300

    First shot. Looping all the way to the number given.

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,7 +1,16 @@
-def generate_prime_numbers(start, finish):
-	pass
+def generate_prime_numbers(finish):
+    for number in range(0, finish):
+        if isPrime(number):
+            print number
 
 
 def isPrime(number):
-	pass
-	
+    if number < 2:
+        return False
+    if number == 2:
+        return True
+    else:
+        for count in range(2, number):
+            if number % count == 0:
+                return False
+            return True
