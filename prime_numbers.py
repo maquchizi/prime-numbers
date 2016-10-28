5512b6597bef6b41239e8a8a9dc4f09642ad00e2
Mark Ng'ang'a <maqnganga@gmail.com>
2016-10-28 16:57:55 +0300

    Now testing only upto square root of number given. Much faster.

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def generate_prime_numbers(finish):
     for number in range(0, finish):
         if isPrime(number):
@@ -10,7 +13,7 @@ def isPrime(number):
     if number == 2:
         return True
     else:
-        for count in range(2, number):
+        for count in range(2, int(sqrt(number))):
             if number % count == 0:
                 return False
             return True
