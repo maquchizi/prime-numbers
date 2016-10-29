7f47645e82441e2fe05bf2c791c777fe709abb49
Mark Ng'ang'a <maqnganga@gmail.com>
2016-10-29 23:34:49 +0300

    Added comments

--- prime_numbers.py
+++ prime_numbers.py
@@ -2,17 +2,30 @@ from math import sqrt
 
 
 def generate_prime_numbers(finish):
+    '''
+        Loop through all number from 0 to finish
+        Check if number is prime
+        Print it if it is otherwise, ignore
+    '''
     for number in range(0, finish):
-        if isPrime(number):
+        if is_prime(number):
             print number
 
 
-def isPrime(number):
+def is_prime(number):
+    '''
+        Check if a number is a prime
+        Return True if it is otherwise, False
+    '''
     if number < 2:
         return False
     if number == 2:
         return True
     else:
+        '''
+            For speed, only test till sqrt of number
+            If no match found in this first partion, there will be none in the next partition till number
+        '''
         for count in range(2, int(sqrt(number))):
             if number % count == 0:
                 return False
